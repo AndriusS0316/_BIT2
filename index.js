2e781261c19c0dc54bf4d21511b978fd1f2dc463
Andrius <andriuss0316@gmail.com>
2021-06-08 18:54:43 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var sk1; // cia deklaravau kintamaji sk1 Galinčios būti kintamojo reikšmės a-z A-Z 0-9 _ $
+var sk2;
+var sk3 = 4;
+sk3 = 2;
+
+var saskaitosSuma;
+var totalInvoiceSum; 
+
+/*
+var kintamojo deklaravimas
+= reiksmes priskyrimas
++ suma
+- atimtis
+* daugyba
+/ dalyba
+*/
+
+sk1 = 5;
+sk2 = 7;
+var rez;
+
+rez = sk1 + sk2 / sk3 * sk1;
+
+
+console.log(rez);
+
