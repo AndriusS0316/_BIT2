4cfacf92bf88938b493d268cffd1beb5813545d2
Andrius <andriuss0316@gmail.com>
2021-06-09 14:33:18 +0300

    1

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ rez = zz + ni - rt * qw / e99
 console.log(rez)
 
 
+
+
 var r5 = 55;
 var r4 = 65;
 var niks = 77;
@@ -67,3 +69,51 @@ rez = r5 + r4 - niks
 //    55 + 65 - 77 
 //        43
 console.log(rez)
+
+
+
+var tekstas = "cia yra tekstas"; //kabutese yra "stringas" bet gali buti ir viengubose kabutese
+var kitas = '"specialus" tekstas su kitom kabutem';
+var vienas = "tekstas \\ \"'te  k\tstas'\" \\\\ tek\r\nstas \u4EFB"
+console.log(tekstas);
+console.log(kitas);
+console.log(vienas);
+
+
+var tekstas = "Labas";
+tekstas = tekstas + " pasauli";
+
+var s1 = "15";
+var s2 = "5";
+
+console.log(tekstas);
+console.log(s1 + s2);
+
+var a1 = 15;
+var a2 = 5; 
+
+console.log(a1 + a2)
+
+console.log("" + a1 + a2 + s1 + s2);
+
+console.log(s1 + s2 + a1 + a2);
+
+// boolean true/false
+
+// var b = true
+// var b2 = false
+
+
+var sk1 = 55;
+
+if (sk1 > 10) {
+    console.log("didelis");
+    sk1 = sk1 * 10;
+    console.log(sk1);
+} else {
+    console.log("mazas");
+    sk1 = 10 / sk1;
+    console.log(sk1);
+    console.log("dar viena komanda");
+}
+console.log("pabaiga");
