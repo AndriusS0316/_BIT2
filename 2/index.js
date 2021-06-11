4131fb441a104b6dbada2dda75e7becbc86d1d48
Andrius <andriuss0316@gmail.com>
2021-06-11 13:26:43 +0300

    1

--- 2/index.js
+++ 2/index.js
@@ -2,15 +2,9 @@ var suma = 0;
 
 var x = 100; // max skaicius iki kurio norime surasti prime
 
-
-
-for (var i = 2; i <= x; i++) { // for loop - var i=2 bus ivykdytas viena karta pries vykdant viso bloko operacija; i <= x - nustato kondicija vykdant bloko op; i++ - bus padaroma kiekviena karta po bloko operacijos (i++ - inkrementuoja po viena +1)
-
-
+for (var i = 2; i <= x; i++) { // for loop - var i = 2 bus ivykdytas viena karta pries vykdant viso bloko operacija; i <= x - nustato kondicija vykdant bloko op; i++ - bus padaroma kiekviena karta po bloko operacijos (i++ - inkrementuoja po viena +1)
 
     var isPrime = true;
-
-
 
     for (var j = 2; j < i; j++) { // 
 
