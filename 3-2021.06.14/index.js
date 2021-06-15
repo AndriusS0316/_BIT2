3437e53d08e36ddca5cd5c6aa0b5567d5b448687
Andrius <andriuss0316@gmail.com>
2021-06-15 13:39:07 +0300

    1

--- 3-2021.06.14/index.js
+++ 3-2021.06.14/index.js
@@ -90,3 +90,81 @@ while (nuo <= iki) {
 }
 console.log("pabaiga");
 
+
+
+
+var nuo = 5;
+var iki = 17;
+var a = nuo;
+
+do { 
+
+    var b = nuo;
+
+    do {
+        console.log(a, b, a*b);
+        b+=1;
+    } while (b<=iki);
+      a+= 1;
+} while (a<= iki);
+
+console.log("Finito");
+
+
+
+
+var sk = 3;
+
+sk++;
+sk += 1;
+sk = sk + 1;
+++sk;
+
+var e = 3;
+
+var kitas = 5 + ++e;
+
+console.log(e);
+console.log(kitas);
+
+
+
+
+var sk = 3;
+var kitas = 5 + ++sk;
+// padidinam sk 1-u -> sk: 4
+// paimam sk reiksme (4)
+// 5 + 4
+// 9
+// i kitas padedam 9
+// var kitas = 5 + sk++;
+// paimam sk reiksme (3) ir atsimenam
+// padidinam sk 1-u -> sk: 4
+// 5 + 3
+// 8
+// i kitas padedam 8
+console.log(sk);
+console.log(kitas);
+
+console.log("n");
+
+var i = 1;
+while (i < 3) {
+    console.log(i++);
+}
+
+
+for (var i = 1; i < 4; i++) {
+    console.log(i);
+}
+
+// N. D.
+
+var nuo = 1;
+var iki = 5;
+
+// daugybos lentele su ciklais "for"
+
+// su ciklus "for" atspausdinti skaicius nuo 100 iki 1 kas 3
+// 100 97 94 91...
+
