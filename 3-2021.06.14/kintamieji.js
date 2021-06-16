b0ad577cc4cbc0e8c50b6cc43593481e9ef3a160
Andrius <andriuss0316@gmail.com>
2021-06-16 13:41:40 +0300

    1

--- 3-2021.06.14/kintamieji.js
+++ 3-2021.06.14/kintamieji.js
@@ -1,13 +1,31 @@
 "use strict";
 
-var sk = 10;
+// var sk = 10;
 
-console.log(sk);
+// console.log(sk);
 
-console.log(nera);
+// console.log(nera);
 
-let naujas = 2;
+// let naujas = 2;
 // deklaravo kintamaji ir priskyre reiksme 2
 
-const konstanta = 15;
+// const konstanta = 15;
 
+// let sk;
+
+// let i = 1;
+// while (i < 3) {
+//     let k = 5;
+//     console.log(i++ * k);
+// }
+// console.log(i);
+// console.log(k);
+
+
+
+for (let i = 1; i < 3; i++) {
+    let k = 5;
+    console.log(i * k);
+}
+
+// console.log(i);
