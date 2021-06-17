ccb8491a5da941eaf6bdf33a581d6ef4e607a372
Andrius <andriuss0316@gmail.com>
2021-06-17 12:49:30 +0300

    1

---
+++ 3-2021.06.14/obi.js
@@ -0,0 +1,39 @@
+"use strict"
+
+// obijektai
+
+let puodukas = {
+    turis: 300,
+    kiekis: 200,
+    spalva: "rudas"
+};
+
+console.log(puodukas.gerimas);
+
+puodukas.gerimas = "kava";
+
+// let tasPatsPuodukas = puodukas;
+
+// puodukas.kiekis -= 50;
+
+// tasPatsPuodukas.spalva = "zalia"
+
+// console.log(puodukas.kiekis);
+// console.log(puodukas.spalva);
+// console.log(puodukas.spalva);
+
+// tasPatsPuodukas.kiekis += 100;
+
+// console.log(puodukas.kiekis);
+
+
+// //console.log(puodukas);
+
+
+// // palyginimo operatoriai
+
+// if (puodukas === tasPatsPuodukas) {
+//     console.log("tas pats puodukas");
+// } else {
+//     console.log("kitas objekas");
+// }
