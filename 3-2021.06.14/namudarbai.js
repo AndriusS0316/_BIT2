6cc92e5a9a59ac2969e82c84ee23bc4ef3b0ed4a
Andrius <andriuss0316@gmail.com>
2021-06-18 16:29:05 +0300

    1

---
+++ 3-2021.06.14/namudarbai.js
@@ -0,0 +1,27 @@
+"use strict"
+
+
+//1. namu darbas:
+
+let st1 = [
+    [1, 2, 3, 5, 3, -4, 2],
+    [4, 5, 6, 10, 11, 12],
+    [7, 8, 9, 2, 6, -3, 67],
+    [7, 8, 9, -12, 3, -18]
+];
+/*
+atspausdinti kiekvienos eilutes elementu vidurki
+atspausdinti visu elementu vidurki
+*/
+
+
+//2. Namu Darbas.
+let m1 = [5, 87, -5, 3, -4];
+/*
+surusiuoti masyva didejimo tvarka
+atspausdinti
+*/
+/*
+surusiuoti masyva mazejimo tvarka
+atspausdinti
+*/
