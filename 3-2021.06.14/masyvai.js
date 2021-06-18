b2d34195ba9b40dd9d47f7cb0e0f137c89438bde
Andrius <andriuss0316@gmail.com>
2021-06-18 16:27:12 +0300

    1

--- 3-2021.06.14/masyvai.js
+++ 3-2021.06.14/masyvai.js
@@ -2,55 +2,53 @@
 
 let m = [5, 87, "labas", false, "!", 5, "pasauli", "aha", 3, true]; //specialus objektas - masyvas. Masyvo elementai numeruojami nuo nulio.
 
-// // masyvas yra objektas. Tai yra svarbu.
+// masyvas yra objektas. Tai yra svarbu.
 
-// //masyvas turi specialia savybe "length", kuri apsprendzia kiek elementu yra masyve.
+//masyvas turi specialia savybe "length", kuri apsprendzia kiek elementu yra masyve.
 
-// console.log(m);
+console.log(m);
 
-// console.log(m[2]);
+console.log(m[2]);
 
-// m[1] = true;
+m[1] = true;
 
-// console.log(m);
-// console.log(m.length);
+console.log(m);
+console.log(m.length); 
 
-// let km = m;
+let km = m;
 
-// km[0] = "pakeiciau";
-// console.log(m[0]);
+km[0] = "pakeiciau";
+console.log(m[0]);
 
-// for (let i = 0; i < m.length; i++) {
-//     console.log(m[i]);
-// }
+for (let i = 0; i < m.length; i++) {
+     console.log(m[i]);
+}
 
-// let suma = 0;
+let suma = 0;
 
-
-
-// for (let i = 0; i < m.length; i++) {
-//     suma = m[i] + suma;
-// }
-// console.log(suma);
+for (let i = 0; i < m.length; i++) {
+    suma = m[i] + suma;
+}
+console.log(suma);
 
 
-// //Namu Darbas.
-// let m1 = [5, 87, -5, 3, -4];
-// /*
-// surusiuoti masyva didejimo tvarka
-// atspausdinti
-// */
-// /*
-// surusiuoti masyva mazejimo tvarka
-// atspausdinti
-// */
+//Namu Darbas.
+let m1 = [5, 87, -5, 3, -4];
+/*
+surusiuoti masyva didejimo tvarka
+atspausdinti
+*/
+/*
+surusiuoti masyva mazejimo tvarka
+atspausdinti
+*/
 
 let m2 = [5, 87, -5, 3, -4, 107];
 
-let max = m[0];
-for (let i = 1; i < m.length; i++) {
-    if (m[i] > max) {
-        max = m[i];
+let max = m2[0];
+for (let i = 1; i < m2.length; i++) {
+    if (m2[i] > max) {
+        max = m2[i];
     }
 }
 console.log(max);
@@ -59,8 +57,11 @@ console.log(max);
 
 let a = 5;
 let b = 10;
+let c = 5;
 
 a = b;
+b = c; 
+
 console.log(a);
 
 console.log(b);
@@ -73,10 +74,13 @@ let st = [
 ];
 
 for (let i = 0; i < st.length; i++) {
-    for (let j = 0; i < st[i].length; j++) {
+    for (let j = 0; j < st[i].length; j++) {
         console.log(st[i][j]);
     }
 }
+
+
+
 
 //namu darbas:
 
@@ -85,8 +89,8 @@ let st1 = [
     [4, 5, 6, 10, 11, 12],
     [7, 8, 9, 2, 6, -3, 67],
     [7, 8, 9, -12, 3, -18]
-    ];
-    /*
-    atspausdinti kiekvienos eilutes elementu vidurki
-    atspausdinti visu elementu vidurki
-    */
+];
+/*
+atspausdinti kiekvienos eilutes elementu vidurki
+atspausdinti visu elementu vidurki
+*/
