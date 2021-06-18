f849fb4b656833afb39120d63f0e328e06b2632b
Andrius <andriuss0316@gmail.com>
2021-06-18 13:35:10 +0300

    1

---
+++ 3-2021.06.14/masyvai.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let m = [5, 87, "labas", false, "!", 5]; //specialus objektas - masyvas. Masyvo elementai numeruojami nuo nulio.
+
+// masyvas yra objektas. Tai yra svarbu.
+
+console.log(m);
+
+console.log(m[2]);
+
+m[1] = true;
+
+console.log(m);
