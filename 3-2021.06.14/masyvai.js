69a9962a51a5f3ab4c499d7421c19340802848c3
Andrius <andriuss0316@gmail.com>
2021-06-18 14:12:11 +0300

    1

--- 3-2021.06.14/masyvai.js
+++ 3-2021.06.14/masyvai.js
@@ -1,8 +1,10 @@
 "use strict"
 
-let m = [5, 87, "labas", false, "!", 5]; //specialus objektas - masyvas. Masyvo elementai numeruojami nuo nulio.
+let m = [5, 87, "labas", false, "!", 5, "pasauli", "aha", 3, true]; //specialus objektas - masyvas. Masyvo elementai numeruojami nuo nulio.
 
 // masyvas yra objektas. Tai yra svarbu.
+
+//masyvas turi specialia savybe "length", kuri apsprendzia kiek elementu yra masyve.
 
 console.log(m);
 
@@ -11,3 +13,35 @@ console.log(m[2]);
 m[1] = true;
 
 console.log(m);
+console.log(m.length);
+
+let km = m;
+
+km[0] = "pakeiciau";
+console.log(m[0]);
+
+for (let i = 0; i < m.length; i++) {
+    console.log(m[i]);
+}
+
+let suma = 0;
+
+
+
+for (let i = 0; i < m.length; i++) {
+    suma = m[i] + suma;
+}
+console.log(suma);
+
+
+//Namu Darbas.
+let m = [5, 87, -5, 3, -4];
+/*
+surusiuoti masyva didejimo tvarka
+atspausdiniti
+*/
+/*
+surusiuoti masyva mazejimo tvarka
+atspausdiniti
+*/
+
