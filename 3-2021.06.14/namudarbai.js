e7758ea1893e09d0869ce2951d3e836a372d73da
Andrius <andriuss0316@gmail.com>
2021-06-20 18:06:17 +0300

    1

--- 3-2021.06.14/namudarbai.js
+++ 3-2021.06.14/namudarbai.js
@@ -76,10 +76,3 @@ atspausdinti
 let lowestToHighest = m1.sort((a, b) => a - b);
 console.log(lowestToHighest);
 
-/*
-surusiuoti masyva mazejimo tvarka
-atspausdinti
-*/
-
-let highestToLowest = m1.sort((a, b) => b - a);
-console.log(highestToLowest);
