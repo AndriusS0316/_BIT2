4a07bf9bfaec7b84403a1df120f1521b6186f870
Andrius <andriuss0316@gmail.com>
2021-06-21 14:45:31 +0300

    1

--- 3-2021.06.14/namudarbai.js
+++ 3-2021.06.14/namudarbai.js
@@ -93,18 +93,18 @@ let st = [
 [7, 8, 9, 2, 6, -3, 67],
 [7, 8, 9, -12, 3, -18]
 ];
-let suma = 0;
+let suma1 = 0;
 let skKiekis = 0;
 for(let y = 0; y < st.length; y++) {
 let eilutesSuma = 0;
 for(let x = 0; x < st[y].length; x++) {
 eilutesSuma += st[y][x];
-suma += st[y][x];
+suma1 += st[y][x];
 }
 skKiekis += st[y].length;
 console.log(eilutesSuma / st[y].length);
 }
-console.log(suma / skKiekis);
+console.log(suma1 / skKiekis);
 
 //rusiavimas
 
@@ -136,3 +136,5 @@ let rez = [
 // 45123
 // 34512
 // 23451
+
+for (let ee = 0; ee < )
