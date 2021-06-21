22c8c880002f45ac221a13c9b9730a435d8dea27
Andrius <andriuss0316@gmail.com>
2021-06-21 14:39:34 +0300

    1

--- 3-2021.06.14/funkcijos.js
+++ 3-2021.06.14/funkcijos.js
@@ -1,26 +1,26 @@
 "use strict";
 
-// let kamuoliukai = [3, 5.4, 2.3, 4.9, 0.1, 0.23];
+let kamuoliukai = [3, 5.4, 2.3, 4.9, 0.1, 0.23];
 
-// for (let i = 0; i < kamuoliukai.length - 1; i++) {
-//     for (let j = i + 1; j < m.length; j++) {
-//         if (m[i] > m[j]) {
-//             let tmp = kamuoliukai[i];
-//             m[i] = m[j];
-//             m[j] = tmp;
-//         }
-//     }
-// }
+for (let i = 0; i < kamuoliukai.length - 1; i++) {
+    for (let j = i + 1; j < kamuoliukai.length; j++) {
+        if (kamuoliukai[i] > kamuoliukai[j]) {
+            let tmp = kamuoliukai[i];
+            kamuoliukai[i] = kamuoliukai[j];
+            kamuoliukai[j] = tmp;
+        }
+    }
+}
 
 
-// let kamuoliukai = [3, 5.4, 2.3, 4.9, 0.1, 0.23];
-// for (let i = 0; i < kamuoliukai.length; i++) {
-// console.log(turis(kamuoliukai[i]));
-// }
-// function turis(r) {
-// let t = 4 * 3.14 * r * r * r / 3;
-// return t;
-// }
+let kamuoliukai1 = [3, 5.4, 2.3, 4.9, 0.1, 0.23];
+for (let i = 0; i < kamuoliukai.length; i++) {
+console.log(turis(kamuoliukai[i]));
+}
+function turis(r) {
+let t = 4 * 3.14 * r * r * r / 3;
+return t;
+}
 
 
 
