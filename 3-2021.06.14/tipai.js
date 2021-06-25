ed42060f7c50e2f014800a680e052b451d1929d2
Andrius <andriuss0316@gmail.com>
2021-06-25 15:13:07 +0300

    1

---
+++ 3-2021.06.14/tipai.js
@@ -0,0 +1,19 @@
+let undef;
+let sk = 10;
+let o = {};
+let f = function () {};
+let str = "tekstas";
+let n = null; 
+
+console.log(typeof undef);
+console.log(typeof sk);
+console.log(typeof o);
+console.log(typeof f);
+console.log(typeof str);
+
+// sk = "10"
+
+if (typeof sk === "number") {
+    console.log(sk * sk);
+}
+
