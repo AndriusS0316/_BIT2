f41fc9f4342984463bc0f0187e803df9b162ad2b
Andrius <andriuss0316@gmail.com>
2021-06-28 14:04:33 +0300

    1

--- 3-2021.06.14/obj2.js
+++ 3-2021.06.14/obj2.js
@@ -1,35 +1,33 @@
 "use strict";
-let masina1 = {
-    pavadinimas: "pirma",
-    kelias: 0,
-    greitis: 0,
-    gazas: function (kiek) {
+
+// darbas sudeti visas savybes skirtas naujam objektui 
+function Masina(pavadinimas) {
+    this.pavadinimas = pavadinimas;
+    this.kelias = 0;
+    this.greitis = 0;
+    this.gazas = function (kiek) {
         this.greitis += kiek;
-    },
-    stabdis: function (kiek) {
+    };
+    this.stabdis = function (kiek) {
         this.greitis -= kiek;
         if (this.greitis < 0) {
             this.greitis = 0;
         }
-    }
-};
-let masina2 = {
-    pavadinimas: "antra",
-    kelias: 0,
-    greitis: 0,
-    gazas: function (kiek) {
-        this.greitis += kiek;
-    },
-    stabdis: function (kiek) {
-        this.greitis -= kiek;
-        if (this.greitis < 0) {
-            this.greitis = 0;
-        }
-    }
-};
+    };
+}
+
+// specialusis objektas new sukuria nauja objekta, paleisti funkcija ir tos funkcijos 
+
+let masina1 = new Masina("pirma");
+let masina2 = new Masina("antra");
+
 console.log(masina1);
 console.log(masina2);
+
 masina1.gazas(15);
 masina2.gazas(3);
+
 console.log(masina1);
 console.log(masina2);
+
+// this dar yra vadinamas kontekstiniu kintamuoju. 
