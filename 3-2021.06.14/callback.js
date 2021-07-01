df049ca5480da9a21c49b043644d6538cf21481e
Andrius <andriuss0316@gmail.com>
2021-07-01 12:32:27 +0300

    20210701

--- 3-2021.06.14/callback.js
+++ 3-2021.06.14/callback.js
@@ -5,16 +5,22 @@ console.log("pradzia");
 function test1() {
     console.log("1");
 }    
+
 function test2() {
     console.log("2");
 }
 function test3() {
     console.log("3");
 }
-    
+
 setTimeout(test2, 2);
 setTimeout(test1, 23);
 setTimeout(test3, 1);
+
+for (let i = 0; i < 1000000; i++) {
+    for (let j = 0; j < 10000; j++) {
+    }
+}
 
 console.log("pabaiga");
 
