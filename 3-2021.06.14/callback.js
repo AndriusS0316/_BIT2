afa7209f38e8cda272f1f6f302414e6c90446879
Andrius <andriuss0316@gmail.com>
2021-07-03 11:18:53 +0300

    20210703

--- 3-2021.06.14/callback.js
+++ 3-2021.06.14/callback.js
@@ -45,20 +45,29 @@ let stringas = ["Namu darbai "];
 
 const fs = require("fs");
 
-fs.readFile("4.txt", (err, data) => {
-    stringas = stringas + data; 
-    console.log("" + data);
-    fs.readFile("3.txt", (err, data) => {
-        stringas = stringas + data; 
+let m90 = [
+    [fs.readFile("4.txt", (err, data) => {
+        stringas = stringas + data;
         console.log("" + data);
-        fs.readFile("1.txt", (err, data) => {
-            stringas = stringas + data; 
-            console.log("" + data); 
-            console.log(stringas); 
-            
-        }); 
-    }); 
-}); 
+        fs.readFile("3.txt", (err, data) => {
+            stringas = stringas + data;
+            console.log("" + data);
+            fs.readFile("1.txt", (err, data) => {
+                stringas = stringas + data;
+                console.log("" + data);
+                console.log(stringas);
+            });
+        });
 
-stringas.sort((e1, e2) => e2.length - e1.length); 
-console.log(stringas); 
+    });
+    ],
+    [
+        stringas.sort((e1, e2) => e2.length - e1.length);
+        console.log(stringas);
+    ]
+]
+
+for (let i = 0; i < m90.length; i++) {
+    
+}
+
