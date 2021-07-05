126d1296342653f04d2bea01182dac85d263c423
Andrius <andriuss0316@gmail.com>
2021-07-05 13:25:32 +0300

    20210705

---
+++ 3-2021.06.14/promisai2.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//Aleksandro 
+
+let p = new Promise((resolve, reject) => {
+    console.log("start promise function");
+    setTimeout(() => {
+        if (Math.random() < -1.5) {
+            console.log("resolving promise");
+            resolve("resolve value");
+        } else {
+            console.log("rejecting promise");
+            reject("reject value");
+        }
+    }, 10);
+    console.log("finish promise function");
+});
+const p1 = p.then(
+    value => {
+        console.log("promise was resolved with value " + value);
+        return "new resolve value";
+    }
+);
+const p2 = p1.then(
+    v => {
+        console.log("p2 resolved with: " + v);
+    }
+);
+const p3 = p2.catch(
+    reason => {
+        console.log("promise was rejected with value " + reason);
+        return "new reject value"
+    }
+);
+
+
+console.log("program finished");
